e2a5db0b3385a6e141b1b1fb8954d33ace80d950
Andrew Lamont <andrew.m.lamont@gmail.com>
2016-02-21 21:18:46 -0500

    working on family

--- recon.py
+++ recon.py
@@ -1,60 +1,10 @@
 #!/usr/bin/python
 
 import random
+import sys
+from language import Language
+from family import Family
 
-def swap_cons(lang):
-	index = random.randint(0,len(lang) - 1)
-	a = lang[index]
-	if index > 0:
-		b = lang[index - 1]
-		lang[index - 1] = a
-		lang[index] = b
-	else:
-		b = lang[index + 1]
-		lang[index + 1] = a
-		lang[index] = b
-	return lang
-
-def pretty_print(langdict, fn="tree.dot"):
-	parents = {}
-	for lang in langdict:
-		for daughter in langdict[lang]:
-			parents[str(daughter)] = str(lang)
-	f = open(fn, 'w')
-	f.write('digraph family {')
-	f.write('\n')
-	for x in parents:
-		s = '\t' + parents[x] + ' -> ' + x + ';'
-		f.write(s)
-		f.write('\n')
-	f.write('}')
-	f.close()
-
-def json_formatter(ranking):
-	out = '{\n'
-	for i in range(len(ranking) - 1):
-		temp = '\t\"' + str(ranking[i]) + '\": [ '
-		for j in range(i,len(ranking)):
-			if i == j:
-				continue
-			temp = temp + '\"{}\"'.format(str(j))
-			if j < len(ranking) - 1:
-				temp = temp + ', '
-		temp = temp + ' ]'
-		if i < len(ranking) - 2:
-			temp = temp + ',\n'
-		out = out + temp
-	out = out + '\n}'
-	return out
-
-def tree2array(tree, langs):
-	towrite = ''
-	outDict = {}
-	for i in range(len(tree)):
-		temp = [str(x) for x in langs[i]]
-		outDict[i] = temp
-	return outDict
-	
 def runMain(numCons, maxLangs, copyProb):
 	protoLang = [str(i) for i in range(numCons)]
 	#print(protoLang)
@@ -95,55 +45,33 @@ def runMain(numCons, maxLangs, copyProb):
 
 	return(langFam, leafs)
 
-def treeToFile(treeArray, fn="langs.txt"):
-	towrite = ""
-	for row in treeArray:
-		towrite += str(row) + ': ' + ' '.join(str(x) for x in treeArray[row])+'\n'
+# Note for later: this is written for artificial data, we'll have to modify this to read actual data from files
 
-	f2 = open(fn, 'w')
-	f2.write(towrite)
-	f2.close()
-	return towrite
+# Default number of constraints -c
+c = 10
+# Default maximum number of languages -l
+l = 25
+# Default probability that a language will be copied -p
+p = .001
 
+# Overwrite defaults with values from command line
+args = sys.argv[1:]
+if '-c' in args:
+	c = int(args[args.index('-c') + 1])
+if '-l' in args:
+	l = int(args[args.index('-l') + 1])
+if '-p' in args:
+	p = float(args[args.index('-p') + 1])
 
-if __name__ == '__main__':
-	import sys
-	from language import Language
+# Generate the constraint set - ints in [1,c]
+constraints = [x for x in range(1, c + 1)]
 
-	# Note for later: this is written for artificial data, we'll have to modify this to read actual data from files
+# Generate random language to use as root for family
+randomroot = Language(constraints)
+randomroot.randomize_ranking()
 
-	# Default values
-	# Number of constraints -c
-	c = 10
-	# Maximum number of languages -l
-	l = 25
-	# Probability that a language will be copied -p
-	p = .001
+# Initialize family
+languagefamily = Family(randomroot)
 
-	# Overwrite defaults with values from command line
-	args = sys.argv[1:]
-	if '-c' in args:
-		c = int(args[args.index('-c') + 1])
-	if '-l' in args:
-		l = int(args[args.index('-l') + 1])
-	if '-p' in args:
-		p = float(args[args.index('-p') + 1])
-
-	# Generate the constraint set - ints in [1,c]
-	constraints = [x for x in range(1, c + 1)]
-
-	# Generate random language to use as root for family
-	randomroot = Language(constraints)
-	print(randomroot.check_for_loops())
-	randomroot.randomize_ranking()
-	for c in randomroot.ranking:
-		print(c,randomroot.ranking[c])
-	print(randomroot.check_for_loops())
-
-	sys.exit()
-
-	(langFam, treeArray) = runMain(args.numCons, args.maxLangs, args.copyProb)
-
-	written = treeToFile(treeArray, fn="langs.txt")
-
-	pretty_print(langFam, fn="tree.dot")
+# Evolve family
+languagefamily.evolve(l,p)
