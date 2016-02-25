6a0efc005fd6e7e75aeb63ebfc24c0e2df54acca
andrew <alamont@indiana.edu>
2016-02-24 23:33:38 -0500

    debugging

--- evaluation.py
+++ evaluation.py
@@ -1,9 +1,12 @@
 def eval(gold, test):
 	numbersharednodes = 0.0
 	for node in gold:
-		if node in tested:
+		if node in test:
 			numbersharednodes += 1
 	precision = numbersharednodes / len(test)
 	recall = numbersharednodes / len(gold)
-	fscore = 2 * precision * recall / (precision + recall)
+	if precision + recall > 0:
+		fscore = 2 * precision * recall / (precision + recall)
+	else:
+		fscore = 0
 	return [precision, recall, fscore]
