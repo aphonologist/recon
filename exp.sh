9a8cba2ab958d8f3b89c1b16765d084809858e36
Andrew Lamont <alamont@login2.bigred2.uits.iu.edu>
2016-03-06 19:05:58 -0500

    now?

--- exp.sh
+++ exp.sh
@@ -8,7 +8,7 @@ for probability in 0.1 0.01 0.001 & do
 	for constraints in 2 4 8 16 32 64 128 & do
 		for langs in 2 4 8 16 32 64 128 & do
 			echo $probability $constraints $langs;
-			python3 recon.py -noheader -n $iterations -p $probability -c $constraints -l $langs >> results.2016-03.dat
+			python3 recon.py -noheader -n $iterations -p $probability -c $constraints -l $langs >> results.2016-03.dat &
 		done;
 	done;
 done;
